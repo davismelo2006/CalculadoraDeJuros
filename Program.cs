11d54af665e047e4428d91737635cdf12d9208c2
Davi Melo <davisantosmelo2006@gmail.com>
2023-06-11 19:52:17 -0300

    calculo de juros simples

--- Program.cs
+++ Program.cs
@@ -20,33 +20,33 @@ namespace AppJuros
             Console.WriteLine("Digite o valor do capital aplicado:");
             Decimal amount = Decimal.Parse(Console.ReadLine()!);
 
-            Console.WriteLine("Digite o valor da taxa de Juros:");
+            Console.WriteLine("Digite o valor da taxa de Juros(somente o número):");
             Decimal interestRate = Decimal.Parse(Console.ReadLine()!);
 
             Console.WriteLine("A taxa é anual ou mensal?");
             Console.WriteLine("1 -> Anual");
             Console.WriteLine("2 -> Mensal");
             string typeOfDeadline = Console.ReadLine()!;
-            Decimal interestTime = 0;
-            // transformar tudo em MESES
-            if (typeOfDeadline == "1")
-            {
-                interestTime = Decimal.Round(interestRate / 12, 2);
-            }
+            // transformar a taxa de quando for anual para mensal
+            Decimal interestPerMonth = typeOfDeadline == "1" ? (interestRate / 12) / 100 : interestRate / 100; // de porcentagem para decimal
 
-            Console.WriteLine("Qual o periodo em meses?");
-            // criar opção em anos
+            Console.WriteLine("Em quantos meses:");
+            // transformar de anos para meses
             int numberOfMonths = int.Parse(Console.ReadLine()!);
 
-            GetInterest(typeOfInterest, amount, interestRate, interestTime, numberOfMonths);
+            GetInterest(typeOfInterest, amount, interestPerMonth, numberOfMonths);
         }
 
-        static void GetInterest(string typeOfInterest, Decimal amount, Decimal interestRate, Decimal interestTime, int numberOfMonths)
+        static void GetInterest(string typeOfInterest, Decimal amount, Decimal interestPerMonth, int numberOfMonths)
         {
             switch (typeOfInterest)
             {
                 case "1":
+                    Decimal finalValue = amount * interestPerMonth * numberOfMonths;
 
+                    Console.WriteLine("---------------------");
+                    Console.WriteLine("O valor de juros: R$" + finalValue.ToString("C"));
+                    Console.WriteLine("O valor total do montante: R$" + (amount + finalValue).ToString("C"));
                     break;
                 case "2":
                     Console.WriteLine("Composto");
